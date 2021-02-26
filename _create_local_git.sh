6ec149338ad00521d791e6f4e5ea1c3f1a67f902
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-02-26 02:39:02 +0100

    Create _create_local_git.sh

---
+++ _create_local_git.sh
@@ -0,0 +1,7 @@
+#!/usr/local/bin/bash
+
+# Setup
+git config --global pull.rebase false
+git config --global user.email "remet@remet.cz"
+git config --global credential.helper store
+git clone https://github.com/remetremet/RMT_ipfw.git /var/db/RMT_firewall
