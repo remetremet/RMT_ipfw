13f5c33c98e16f4eb48edbad704676889f5d8d32
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-03-11 21:17:56 +0100

    Update _update.sh

--- _update.sh
+++ _update.sh
@@ -1,12 +1,12 @@
 #!/usr/local/bin/bash -
 
 # Synchronize scripts from github to local repo
-cd /var/db/RMT_firewall
+cd /var/db/RMT_ipfw
 git pull origin master
 
 # Copy scripts from local repo to working directory (/etc)
-cp /var/db/RMT_firewall/rc.firewall /etc/
-cp /var/db/RMT_firewall/rc.firewall.definitions /etc/
-#cp /var/db/RMT_firewall/rc.firewall.config /etc/
+cp /var/db/RMT_ipfw/rc.firewall /etc/
+cp /var/db/RMT_ipfw/rc.firewall.definitions /etc/
+#cp /var/db/RMT_ipfw/rc.firewall.config /etc/
 chmod 755 /etc/rc.firewall
 chmod 755 /etc/rc.firewall.*
