13db04cff08a528aeb13dbe15cc30effd81e48a6
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-04-30 00:52:07 +0200

    Update _setup.sh

--- _setup.sh
+++ _setup.sh
@@ -1,13 +1,25 @@
 #!/usr/local/bin/bash
 
+BASEPATH="/var/db/RMT_ipfw"
+
 # Software requirements
 pkg install -y bash
+
+# Setup directories
+if [ ! -d "${BASEPATH}" ]; then
+ mkdir "${BASEPATH}"
+ chmod 777 "${BASEPATH}"
+fi
 
 # Setup
 git config --global pull.rebase false
 git config --global user.email "remet@remet.cz"
 git config --global credential.helper store
-git clone https://github.com/remetremet/RMT_ipfw.git /var/db/RMT_ipfw
+git clone https://github.com/remetremet/RMT_ipfw.git "${BASEPATH}/github"
+
+# Copy scripts to working directory
+cp -R ${BASEPATH}/github/_*.sh ${BASEPATH}/
+chmod 755 ${BASEPATH}/_*.sh
 
 echo "-----"
 echo " Do not forget to add cron job via 'crontab -e':"
